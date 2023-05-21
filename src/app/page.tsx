691451137ad259d881bc4f7e1de335c3643fad46
gutovskiy.ru <sergey.gutovsk@gmail.com>
2023-05-21 21:29:26 +0300

    v 1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,147 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import "src/app/global.css";
+
+import { MouseEvent, useState } from "react";
+import styled from "styled-components";
+import Head from "next/head";
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.6);
+
+  cursor: pointer;
+`;
+
+const Modal = styled.form`
+  position: relative;
+  max-width: 400px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  padding: 32px 32px 64px 32px;
+  background-color: white;
+
+  cursor: default;
+
+  @media (max-width: 600px) {
+    margin-top: auto;
+    padding: 20px 20px 40px 20px;
+    border-radius: 16px 16px 0 0;
+  }
+`;
+
+const Title = styled.span`
+  margin-bottom: 12px;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 18px;
+`;
+
+const Description = styled.p`
+  margin-bottom: 28px;
+  text-align: center;
+  font-size: 14px;
+  line-height: 22px;
+`;
+
+const Input = styled.input`
+  margin-bottom: 28px;
+  padding: 12px 20px;
+  font-size: 14px;
+  border: solid 1px #E0E0E0;
+
+  &::placeholder {
+    color: #BEBEBE;
+  }
+`;
+
+const SubmitButton = styled.button`
+  color: white;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 16px;
+  background-color: black;
+
+  cursor: pointer;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 16px;
+  height: 16px;
+
+  cursor: pointer;
+
+  &::before, &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    height: 1px;
+    width: 100%;
+    background-color: black;
+  }
+
+  &::before {
+    transform: translate(-50%, -50%) rotateZ(45deg);
+  }
+
+  &::after {
+    transform: translate(-50%, -50%) rotateZ(-45deg);
+  }
+`;
+
+const Page = () => {
+  const [ isModalOpen, setIsModalOpen ] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const onModalWrapperClicked = (e: MouseEvent<HTMLElement>) => {
+    if (e.target !== e.currentTarget) return;
+    closeModal();
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Head key={"font"}>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>  
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"/>
+      </Head>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <button onClick={openModal}>Открыть</button>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      {
+        isModalOpen &&
+        <ModalWrapper onClick={onModalWrapperClicked}>
+          <Modal>
+            <CloseButton type="button" onClick={closeModal}/>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <Title>Подписка на новости</Title>
+            <Description>
+              Подпишитесь на новостную рассылку IRNBY и получите скидку 10% на первый заказ
+            </Description>
+            
+            <Input placeholder="Введите свой e-mail"/>
+            
+            <SubmitButton>ПОДПИСАТЬСЯ</SubmitButton>
+          </Modal>
+        </ModalWrapper>
+      }
+    </>
+  );
+};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
